d529dd6d5fc4c2bd2aab0b382f9893f22d35b57f
加藤康夫 <yasuokatou@gmail.com>
2021-01-02 08:38:55 +0900

    ブログタグの追加を実装

--- myblog/urls.py
+++ myblog/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('new_blog_content/', views.new_blog_content, name='new_blog_content'),
     path('edit_blog_content/', views.edit_blog_content, name='edit_blog_content'),
     path('get_blog_by_id/', views.get_blog_by_id, name='get_blog_by_id'),
+    path('get_tag_list/', views.get_tag_list, name='get_tag_list'),
+    path('add_tag_list/', views.add_tag_list, name='add_tag_list'),
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -2,40 +2,60 @@ from django.http import HttpResponse, HttpResponseRedirect, QueryDict
 from django.http.response import JsonResponse
 from django.views import generic
 
-from .models import BlogContent
+from .models import BlogContent, BlogTag
 # Create your views here.
 
 class IndexView(generic.ListView):
-    template_name = 'myblog/index.html'
-    context_object_name = 'myblog_content_list'
+  template_name = 'myblog/index.html'
+  context_object_name = 'myblog_content_list'
 
-    def get_queryset(self):
-        return BlogContent.objects.order_by('-create_date')
+  def get_queryset(self):
+    return BlogContent.objects.order_by('-create_date')
 
 def new_blog_content(request):
-    blog = BlogContent(content_title=request.POST['content_title'],
-      content_text=request.POST['content_text'])
-    blog.save()
+  blog = BlogContent(content_title=request.POST['content_title'],
+    content_text=request.POST['content_text'])
+  blog.save()
 
-    return HttpResponseRedirect('/myblog/')
+  return HttpResponseRedirect('/myblog/')
 
 def edit_blog_content(request):
-    blog = BlogContent(id=int(request.POST['myblog-id']),
-      content_title=request.POST['content_title'],
-      content_text=request.POST['content_text'])
-    blog.save()
+  blog = BlogContent(id=int(request.POST['myblog-id']),
+    content_title=request.POST['content_title'],
+    content_text=request.POST['content_text'])
+  blog.save()
 
-    return HttpResponseRedirect('/myblog/')
+  return HttpResponseRedirect('/myblog/')
 
 def get_blog_by_id(request):
-    dic = QueryDict(request.body, encoding='utf-8')
-    # print(dic.get('myblog_id'))
-    rec = BlogContent.objects.get(id=int(dic.get('myblog_id')))
-    json = {
-      "myblog_id": rec.id,
-      "content_title": rec.content_title,
-      "content_text": rec.content_text,
-      "update_date": rec.update_date
-    }
-    return JsonResponse(json)
+  dic = QueryDict(request.body, encoding='utf-8')
+  # print(dic.get('myblog_id'))
+  rec = BlogContent.objects.get(id=int(dic.get('myblog_id')))
+  json = {
+    "myblog_id": rec.id,
+    "content_title": rec.content_title,
+    "content_text": rec.content_text,
+    "update_date": rec.update_date
+  }
+  return JsonResponse(json)
+
+def get_tag_list(request):
+  '''
+  タグの一覧を取得する
+  '''
+  list = []
+  for rec in BlogTag.objects.order_by('tag_text'):
+    list.append({"id": rec.id, "name": rec.tag_text})
+  return JsonResponse({"list": list})
+
+def add_tag_list(request):
+  '''
+  タグを追加する
+  '''
+  dic = QueryDict(request.body, encoding='utf-8')
+  blog = BlogTag(tag_text=dic.get('tag_text'))
+  blog.save()
+
+  # タグの一覧を返す
+  return get_tag_list(request)
 #[EOF]
